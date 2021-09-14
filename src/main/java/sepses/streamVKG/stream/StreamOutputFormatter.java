16cc4ec1decb3f56bd605a002dc627787eef979e
Kabul Kurniawan <kabul.kurniawan@tuwien.ac.at>
2021-09-14 16:35:22 +0200

    add configuration file

--- src/main/java/sepses/streamVKG/stream/StreamOutputFormatter.java
+++ src/main/java/sepses/streamVKG/stream/StreamOutputFormatter.java
@@ -4,6 +4,10 @@ import it.polimi.sr.rsp.csparql.sysout.ConstructResponseDefaultFormatter;
 
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j;
+import org.apache.commons.io.IOUtils;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -25,7 +29,15 @@ public class StreamOutputFormatter extends ConstructResponseDefaultFormatter {
 
     }
 
+    @SneakyThrows
     protected void out(String s) {
-        writer.println(s);
+        Model model = ModelFactory.createDefaultModel()
+                .read(IOUtils.toInputStream(s, "UTF-8"), null, "N-TRIPLES");
+        Property p1 = model.createProperty("http://streamreasoning.org/csparql/eventTime");
+        Property p2 = model.createProperty("http://streamreasoning.org/csparql/processingTime");
+        model.remove(null,p1,null);
+        model.remove(null,p2,null);
+        writer.println(model.write(System.out));
+        model.close();
     }
 }
